af72ead15f261ba248513833f5b401c6d32777a9
Romildo Felix <romildof@gmail.com>
2018-11-27 20:27:01 -0200

    Projeto criado

---
+++ src/couse/Program.java
@@ -0,0 +1,17 @@
+package couse;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in) ;
+	
+		System.out.println("Hello word!");
+		
+		sc.close();
+		
+	}
+
+}
